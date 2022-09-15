2aa43d9d220e4f93012cacb6ea33cce8ba9cc244
seona.bang <sit.or.seona@gmail.com>
2022-09-15 21:53:34 +0900

    220915 10000hours: modalShow() 추가

---
+++ js/10000hours.js
@@ -0,0 +1,4 @@
+// js file
+function modalShow () {   
+    document.querySelector('.cont-modal').style = "display: block; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%);"
+}
